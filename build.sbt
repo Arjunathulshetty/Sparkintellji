1aa62fa76265955e25e2c79a3106d4d0308b1f87
Arjunathulshetty <arjunathulshetty@gmail.com>
2020-12-31 13:41:53 +0530

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   build.sbt
    	new file:   src/main/scala/com/Spark/scala/ABC.scala
    	new file:   src/main/scala/com/Spark/scala/ABCD.sc
    	new file:   src/main/scala/com/Spark/scala/Aggregation.scala
    	new file:   src/main/scala/com/Spark/scala/XYZ.scala

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "untitled"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.0.0"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.0"
+
+
+
---
+++ src/main/scala/com/Spark/scala/XYZ.scala
@@ -0,0 +1,43 @@
+package com.Spark.scala
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+object XYZ {
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "WordCount")
+
+    val name = sc.textFile("../Employee.txt")
+
+    val namePairRDD = name.map(x=> (x.split(",")(0),x.split(",")(1)))
+    //Step 3 : Now swap namePairRDD RDD.
+    //val swapped = namePairRDD.map(item => item.swap)
+    //step 4: Now sort the rdd by key.
+    val sortedOutput = namePairRDD.sortByKey()
+    //Step 5 : Now swap the result back
+    val swappedBack = sortedOutput.map(item => item.swap)
+    val results = swappedBack.collect()
+val result = results.mkString("\n")
+    println(result)
+    results.foreach(println)
+
+    for (x <- results){
+      println(x._1)
+    }
+
+
+    //swappedBack.
+    //val words = swappedBack.map(x => x.split(",")x(0))
+    //Step 6 : Save the output as a Text file and output must be written in a single file.
+    //swappedBack. repartition(1).saveAsTextFile("../result.txt")
+
+
+  }
+
+
+}
