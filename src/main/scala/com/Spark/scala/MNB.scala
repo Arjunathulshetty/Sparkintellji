253baca79aec26bb4ed899bc6b565d44bddc2d4b
Arjunathulshetty <arjunathulshetty@gmail.com>
2021-02-24 11:58:25 +0530

    Changes to be committed: 	modified:   src/main/scala/com/Spark/scala/MNB.scala

--- src/main/scala/com/Spark/scala/MNB.scala
+++ src/main/scala/com/Spark/scala/MNB.scala
@@ -2,6 +2,23 @@ package com.Spark.scala
 
 object MNB {
   def main(args: Array[String]): Unit = {
+    val ass = new Array[Int](2)
+    ass(0) =888
+ass(1)=88
+println(ass(0))
+    for ( m1 <-ass ) {
+
+      println(m1)
+    }
+
+    val annn =new Tuple3[String,Int,String]("arjun",999,"shetty")
+      println(annn._1)
+    ass.foreach(println)
+ val nn = List(1,4,2,3).sorted
+
+
+
+
   }
 
 }
